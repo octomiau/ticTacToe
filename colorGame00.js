46ca0c29b01d3647cd65f08c93ea7a942ff76fd8
Diana <avilesdianaa@gmail.com>
2023-09-03 00:22:24 -0500

    changed color and player to be 0 or 1

--- colorGame00.js
+++ colorGame00.js
@@ -1,12 +1,13 @@
 const Board = () => {
-  const [player, setPlayer] = React.useState(0);
+  const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
   return (
     <div
       className="game-board"
       onClick={(e) => {
-        setPlayer(player + 1);
+        setPlayer((player + 1)% 2); //the player needs to be either 0 or 1
         status = `Player ${player}`;
+        e.target.style.background = "red"; //changed the color using the onclick 
       }}
     >
       <div id="info">

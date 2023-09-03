f7490be6d87da030e3ee9b7c48cff5b4f9330821
Diana <avilesdianaa@gmail.com>
2023-09-03 00:43:42 -0500

    give each square component an ID

--- colorGame00.js
+++ colorGame00.js
@@ -1,15 +1,22 @@
+const Square= ({id}) => {
+  return <button>
+    <h1>{id}</h1>
+    </button>
+};
+
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
+  function renderSquare(i){
+    return <Square id={i}></Square>;
+  }
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        setPlayer((player + 1)% 2); //the player needs to be either 0 or 1
-        status = `Player ${player}`;
-        e.target.style.background = "red"; //changed the color using the onclick 
-      }}
-    >
+    <div className="game-board">
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
